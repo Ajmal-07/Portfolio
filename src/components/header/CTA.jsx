1f1a95a4220e16aa986fcfb11bfcb1fd7ec01ee3
Ajmal-07 <108513358+Ajmal-07@users.noreply.github.com>
2023-04-04 18:23:02 +0530

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import Resume_ajmal from "../../assets/Ui.png"
 const CTA = () => {
   return (
     <div className='cta'>
-        {/* <a href={Resume_ajmal} download className='btn'> Download CV</a> */}
+         <a href={Resume_ajmal} download className='btn'> Download CV</a> 
         {/* <a href={Resume_ajmal} download={Resume_ajmal} className='btn'> Download</a> */}
         {/* <a href='#contact' className='btn btn-primary'>Lets talk</a> */}
     </div>
